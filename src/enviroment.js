8f06ebd343eb99a27cfea8c21575fc0fe8bed4c3
nickdepaolo <79952881+nickdepaolo@users.noreply.github.com>
2021-05-25 15:13:34 -0400

    fix3

--- src/enviroment.js
+++ src/enviroment.js
@@ -5,7 +5,7 @@ switch (window.location.hostname) {
     APIURL = 'http://localhost:3586';
         break;
     case 'nicksclientfinal2021.herokuapp.com':
-    APIURL = 'https://nikcsfinalserver2021.herokuapp.com/'
+    APIURL = 'https://nikcsfinalserver2021.herokuapp.com'
 }
 
 export default APIURL;
